ace8a9a947c7374da0033d4cfd30bdf2124c1e70
Aqil Thanawala <aqil.thanawala@gmail.com>
2021-01-06 11:15:03 -0800

    Remove all files

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Header } from './Header/Header';
-export { default as Image } from './Image/Image';
-export { default as Layout } from './Layout/Layout';
-export { default as Seo } from './Seo/Seo';
