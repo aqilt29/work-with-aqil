868a9352c29748d3700927d033bfccd39770186a
Aqil Thanawala <aqil.thanawala@gmail.com>
2021-07-08 23:22:05 -0700

    links are styled and functional same with the one in the header returns to home

---
+++ src/pages/reading.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../components/layout';
+
+const ReadingPage = () => {
+  return (
+    <Layout>
+      <h1>Hello</h1>
+      <Link to="/">Go Back</Link>
+    </Layout>
+  )
+};
+
+export default ReadingPage;
