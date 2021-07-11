b222e332e27aa898ecf989793de67cfc17e1db58
Aqil Thanawala <aqil.thanawala@gmail.com>
2021-07-11 15:57:04 -0700

    created a contact me email CTA

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -5,6 +5,7 @@ import {
   BioText,
   HeroContainer,
   HeroImg,
+  MailButton,
   
 } from './styles';
 import staticEthGif from '../../images/staticethgif.gif'
@@ -19,6 +20,11 @@ const Hero = () => {
         <BioHeader>What About</BioHeader>
         <BioText>{bio2}</BioText>
         <BioText>{bio}{<a target="__Blank" href="https://finematics.com/history-of-defi-explained" >DeFi Summer</a>}{bio1}</BioText>
+        <MailButton>
+          <a href="mailto:me@workwithaqil.com">
+            Contact Me
+          </a>
+        </MailButton>
       </BioContainer>
     </HeroContainer>
   )
